ced9c2105c74539c95daa8a083010d7f0b96ffec
David Pastor <david.pastor@prestamosprima.com>
2021-10-26 11:45:10 +0200

    Initial commit - set up

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
