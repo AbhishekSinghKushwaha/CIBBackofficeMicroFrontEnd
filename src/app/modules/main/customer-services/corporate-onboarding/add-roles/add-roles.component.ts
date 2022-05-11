dfb373546e43d8b8bb06b239aa701649e3ffe070
Jerry Odhiambo <jerry.odhiambo@azenia.com>
2022-05-11 23:21:29 +0300

    Screen designs for create-profile, add-roles, select-account-limits and users

---
+++ src/app/modules/main/customer-services/corporate-onboarding/add-roles/add-roles.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-roles',
+  templateUrl: './add-roles.component.html',
+  styleUrls: ['./add-roles.component.scss']
+})
+export class AddRolesComponent implements OnInit {
+  rolesForm: FormGroup;
+  panelOpenState = {
+    title: true,
+    roles: true
+  };
+  constructor(
+    private readonly router: Router,
+    private readonly fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.rolesForm = this.fb.group({
+      roleTitle: ['']
+    });
+  }
+
+  saveAndContinue() {}
+}
---
+++ src/app/modules/main/customer-services/corporate-onboarding/add-users/add-users.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/core/services/utils/storage.service';
+
+@Component({
+  selector: 'app-add-users',
+  templateUrl: './add-users.component.html',
+  styleUrls: ['./add-users.component.scss']
+})
+export class AddUsersComponent implements OnInit {
+  panelOpenState = {
+    userDetails: true,
+    initiators: false,
+    products: true
+  };
+
+  roles: any = [];
+  products: any = [];
+  accounts: any = [];
+  constructor(private router: Router, private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    this.roles = this.storageService.getData('onboarding-roles');
+    this.products = this.storageService.getData('products-and-services');
+    this.accounts = this.storageService.getData('user-accounts');
+    console.log(this.accounts);
+  }
+
+  saveAndContinue() {
+    this.router.navigate([
+      '/customer-services/corporate-onboarding/add-workflow'
+    ]);
+  }
+}
