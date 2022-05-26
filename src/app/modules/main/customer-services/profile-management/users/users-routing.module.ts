42dbade35cdc7e0221fe040406ee99e1548e4672
feyisewa <feyisewaawotedu@gmail.com>
2022-05-26 07:35:23 +0200

    added new-user component

--- src/app/modules/main/customer-services/profile-management/users/users-routing.module.ts
+++ src/app/modules/main/customer-services/profile-management/users/users-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewUserComponent } from './new-user/new-user.component';
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [
+  {
+    path: '', component: UsersComponent,
+    children: [
+      { path: '', redirectTo: 'new-user',pathMatch:'full' },
+      {
+        path: 'new-user',
+        component: NewUserComponent
+      },
+      // {
+      //   path: 'file-template-configuration',
+      //   component: FileTemplateComponent,
+      //   children: [
+      //     { path: '', redirectTo: 'list' },
+      //     { path: 'list', component: ListFileTemplatesComponent },
+      //     { path: 'new', component: NewFileTemplatesComponent },
+      //   ]
+      // },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/main/customer-services/profile-management/users/users.module.ts
+++ src/app/modules/main/customer-services/profile-management/users/users.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { MatStyleModule } from 'src/app/mat-style.module';
+import { SharedModalsModule } from 'src/app/shared/modals/shared-modals.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    NewUserComponent
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    MatStyleModule,
+    SharedModalsModule,
+    ReactiveFormsModule
   ]
 })
 export class UsersModule { }
